6eaa7faaadedbb68a74486de942e551867fb0adf
erica35225 <erica35225@gmail.com>
2022-03-21 22:46:56 +0530

    add basic authentication with form based login by spring security with hardcoded user and password

---
+++ src/main/java/com/courses/Config/MyWebSecurity.java
@@ -0,0 +1,41 @@
+package com.courses.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+//@EnableGlobalMethodSecurity(prePostEnabled = true) IF WE WANT TO RESTRICT ACCESS TO AN API BASED ON ROLE
+public class MyWebSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("erica").password(this.passwordEncoder().encode("12345")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("eric").password(this.passwordEncoder().encode("1234")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/public/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf()
+                .disable()
+                .httpBasic();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+}
